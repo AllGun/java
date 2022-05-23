3688049bff0c9c2a9ae63a95be965b5e79d72a4a
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-23 15:04:59 -0300

    StrDemo.java

---
+++ javaBook01/StrDemo.java
@@ -0,0 +1,26 @@
+// Demonstra a sequência de escape em strings.
+
+class StrDemo {
+
+	public static void main (String args []) {
+
+		System.out.println("First line\nSecond line");
+
+		System.out.println("A\tB\tC");
+		System.out.println("A\t B\t C");
+		System.out.println("A \t B \t C");
+		
+		char A = '\t';
+
+		
+
+	
+		System.out.println("D\tE\tF");
+
+System.out.println("B"+A+"J");
+System.out.println("B\tJ");
+
+
+	}
+
+}
