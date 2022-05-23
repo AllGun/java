6f6c903937b4dfcbd769b9dd0e8e462adb68904c
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-23 10:16:44 -0300

    CharArithDemo.java

---
+++ javaBook01/CharArithDemo.java
@@ -0,0 +1,22 @@
+// Variáveis de caracteres podem ser tratadas como inteiros.
+
+class CharArithDemo {
+
+	public static void main (String args [ ]) {
+
+		char ch;
+
+		ch = 'X';
+		System.out.println("ch contains " + ch);
+
+		ch++; // incrementa ch (Um char pode ser incrementado)
+		System.out.println("ch is now " + ch);
+
+		ch = 90; // dá a ch o valor z 
+		//Um char pode receber valor inteiro e seu retorno será um caractere
+		System.out.println("ch is now " + ch);
+
+	}
+}
+
+	
