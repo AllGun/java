5f8f187d9d321439a7d4f8d858502a534c098359
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-24 09:34:24 -0300

    LtoD.java

---
+++ javaBook01/LtoD.java
@@ -0,0 +1,18 @@
+// Demonstra a conversão automática de long para double
+
+class LtoD {
+
+	public static void main (String args [ ]) {
+		
+		long L;
+
+		double D;
+
+		L = 100123285L;
+		D = L; // Conversão automática de long para double
+
+		System.out.println("L and D: " + L + " " + D);
+
+	}
+
+}
