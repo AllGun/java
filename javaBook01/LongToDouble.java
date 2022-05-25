afdf09efccb2da9a0b935b8e028a05311deb1d9c
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-25 16:53:00 -0300

    LongToDouble.java

---
+++ javaBook01/LongToDouble.java
@@ -0,0 +1,23 @@
+// *** Esse programa não será compilado. ***
+
+class LongToDouble {
+
+	public static void main (String args []) {
+
+		long L;
+	
+		double D;
+
+		
+		D = 100123285.0;
+
+		L = D; // Inválido!!!!
+
+
+		System.out.println(" L and D: " + L + " " + D);
+
+
+	}
+
+
+}
