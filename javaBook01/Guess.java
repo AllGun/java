689874c1b31a44e770a4d23e954024f9d7a35a34
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-05-28 23:51:11 -0300

    Guess2.java

---
+++ javaBook01/Guess.java
@@ -0,0 +1,14 @@
+public class Guess {
+    throws java.io.IOException {
+
+            char ch, answer = 'k';
+
+            System.out.println("I'm thinking of a letter between A and Z.");
+            System.out.println("Can you guess it: ");
+
+            ch = (char) Sout.in.read();
+
+            if( ch == answer)System.out.println("**Right**");
+        }
+    }
+
---
+++ javaBook01/Guess2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import javax.sound.sampled.SourceDataLine;
+
+// Advinha a letra do jogo, 2a versão
+
+class Guess2 {
+    public static void main(String[] args)
+        trhow java.io.IOException {
+            
+            char ch, answer = 'k';
+
+            System.out.println("I'm thinking of a letter berween A and Z.");
+            System.out.println("Can you guess it: ");
+
+            ch = (char) System.in.read();
+
+            if (ch)
+    }
+}
